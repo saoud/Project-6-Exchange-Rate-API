f85d5710ff53a012d53b89f7c287a44bf07e7d15
Saoud <saoud@saoud.dev>
2021-03-28 02:14:27 -0700

    removing __tests__ folder and adding some logic to main.js

--- src/main.js
+++ src/main.js
@@ -2,3 +2,10 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeRate from './exchangeRateAPI'
+
+function getElements(response) {
+  if (response.result === "success") {
+    
+  }
+}
