31dcb17347f805f3b0525eb6d19baff5b95ac630
Saoud <saoud@saoud.dev>
2021-03-28 01:59:42 -0700

    adding currency input and amount to exchangeRateAPI and the API call

--- src/js/exchangeRateAPI.js
+++ src/js/exchangeRateAPI.js
@@ -1,7 +1,7 @@
 
 export default class ExchangeRate {
   static getRate(currency) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/EUR/GBP/AMOUNT`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}//pair/USD/${currency}/${amount}`)
     .then(function(response){
       if (response.json().result === "error") {
         throw Error(response.json().error-type);
