c564fabf094bf066da915b6be7ae932652874c1c
Saoud <saoud@saoud.dev>
2021-04-02 16:15:38 -0700

    changing some varible names to be consistant

--- src/main.js
+++ src/main.js
@@ -4,8 +4,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRate from './exchangeRateAPI'
 
+function clearFields() {
+  $('.showErrors').text("");
+  $('.showRate').text("");
+  $('.showResultErrors').text("");
+}
+
 function getElements(response) {
-  if (response.result === "success") {
-    
+if (response.result === "success") {
+    $('.showRate').text('Your USD conversion is ${response.conversion_result} ${response.target_code}')
+  } else if (response["error-type"] === "unsupported-code") {
+    $('.showResulError').text("Unfortunatly we have encoutered an error")
+  } else {
+    $('.showErrors').text(`There was an error ${response.message}. Please enter a valid number.`);
   }
 }
+
+$(document).ready(function() {
+  $('#submitExchange').click(function() {
+    let currency = $('#currency').val();
+    let amount = parseInt($('input#inputUSD').val());
+    clearFields();
+    ExchangeRate.getRate(currency, amount).then(function(response) {
+      getElements(response);
+    });
+  });
+});
