7ed50741e762696c208bb375a7b661c798bb7bab
Saoud <saoud@saoud.dev>
2021-04-02 15:48:55 -0700

    fix typos in exchangeRateAPI.js

--- src/exchangeRateAPI.js
+++ src/exchangeRateAPI.js
@@ -1,14 +1,14 @@
 export default class ExchangeRate {
-  static getRate(currency) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}//pair/USD/${currency}/${amount}`)
-    .then(function(response){
-      if (response.json().result === "error") {
-        throw Error(response.json().error-type);
-      }
-      return response.json();
-    })
-    .catch(function(error) {
-      return Error(error);
-    })
+  static getRate(currency, amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.status.Text);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
   }
 }
