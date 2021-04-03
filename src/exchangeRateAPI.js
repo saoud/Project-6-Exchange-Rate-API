148b72f8fca05b371028d5d6d2a6e95781ffaf88
Saoud Rana <saoud@saoud.dev>
2021-04-03 02:06:59 -0700

    fix spacing

--- src/exchangeRateAPI.js
+++ src/exchangeRateAPI.js
@@ -1,14 +1,14 @@
 export default class ExchangeRate {
   static getRate(currency, inputUSD) {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${inputUSD}`)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.status.Text);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      });
-  }
+    .then(function(response) {
+      if (!response.ok) {
+        throw Error(response.status.Text);
+      }
+      return response.json();
+    })
+    .catch(function(error) {
+      return error;
+    });
+}
 }
