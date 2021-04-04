5e9fe98927c84589098cd0e86e8eca301324549b
Saoud Rana <saoud@saoud.dev>
2021-04-03 20:51:15 -0700

    fixing backticks into single quotes as well as changing the class to match the html

--- src/exchangeRateAPI.js
+++ src/exchangeRateAPI.js
@@ -1,6 +1,6 @@
 export default class ExchangeRate {
   static getRate(currency, inputUSD) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${inputUSD}`)
+    return fetch('https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${inputUSD}')
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.status.Text);
