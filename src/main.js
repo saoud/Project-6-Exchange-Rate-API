254256069a4fea7938480510a652cdf16c3fb826
Saoud Rana <saoud@saoud.dev>
2021-04-03 21:19:00 -0700

    removing console.log && a typo for showResultErrors

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ function getElements(response) {
   if (response.result === "success") {
     $('.showResult').text(`Your USD conversion is ${response.conversion_result} ${response.target_code}`);
   } else if (response["error-type"] === "unsupported-code") {
-    $('.showResutlError').text("Unfortunatly we have encoutered an error");
+    $('.showResultErrors').text("Unfortunatly we have encoutered an error");
   } else {
     $('.showErrors').text(`There was an error ${response.message}. Please enter a valid number.`);
   }
@@ -24,8 +24,6 @@ $(document).ready(function() {
   $('#submitExchange').click(function() {
     let inputUSD = $('input#inputUSD').val();
     let currency = $('#currency').val();
-    console.log(inputUSD);
-    console.log(currency);
     clearFields();
     ExchangeRate.getRate(currency, inputUSD).then(function(response) {
       getElements(response);
